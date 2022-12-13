111f746290f03800155568279014d32c40d03c52
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-12-13 18:01:35 +0100

    modif vite

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 Route::get('{any}', function () {
-    return view('index');
+    return view('welcome');
 })->where('any', '.*');
