cf4b2087d8fc66036b84c0c26d87acf27e74c025
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-12-13 17:52:21 +0100

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
 <body style="margin: 0">
 <div id="app">
